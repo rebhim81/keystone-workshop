5cd7e27cf8ba6b42e94d947f6bcbe1f19d12b286
danrinkes@gmail.com <danrinkes@gmail.com@a09e4284-d4b2-6d76-6fd3-1fe43d9ad136>
2012-03-07 14:17:09 +0000

    Tagged versions for San Diego Training

---
+++ tags/TAG_SAN_DIEGO_02_14_2012/examples/SrioFFT/.config/xconfig_SRIOMulticore6/package/package.defs.h
@@ -0,0 +1,13 @@
+/*
+ *  Do not modify this file; it is automatically 
+ *  generated and any modifications will be overwritten.
+ *
+ * @(#) xdc-x14
+ */
+
+#ifndef xconfig_SRIOMulticore6__
+#define xconfig_SRIOMulticore6__
+
+
+
+#endif /* xconfig_SRIOMulticore6__ */ 
