b83980fa885d5d9a35ebce0dd1c73e105dde3981
danrinkes@gmail.com <danrinkes@gmail.com@a09e4284-d4b2-6d76-6fd3-1fe43d9ad136>
2012-06-13 18:22:45 +0000

    Creating Release Branch

---
+++ TAG_CREATE_RELEASE_BRANCH/examples/SrioFFT/include/slaveTask.h
@@ -0,0 +1,70 @@
+/*
+ * slaveTask.h
+ *
+ * Copyright (C) 2012 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  =========================== REVISION INFORMATION ===========================
+ *  Revision Information
+ *   Changed: $LastChangedDate: 2012-02-09 10:29:24 -0500 (Thu, 09 Feb 2012) $
+ *   Revision: $LastChangedRevision: 19 $
+ *   Last Update By: $Author: danrinkes $
+ *  ============================================================================
+ *
+        1         2         3         4         5         6          7
+12345678901234567890123456789012345678901234567890123456789012345678901234567890
+*/
+
+#ifndef SLAVETASK_H_
+#define SLAVETASK_H_
+#include <c6x.h>
+#include <commonTask.h>
+
+/* SRIO Driver Include File */
+#include <ti/drv/srio/srio_drv.h>
+
+#define   PROCESSING_BASE			30
+#define   PROCESSING_INTERVAL		200
+#define   DATA_ARRIVE_BASE			5
+#define   DATA_ARRIVE_INTERVAL		2
+
+typedef enum {
+	SLAVE_READY,
+	SLAVE_WAITING,
+	SLAVE_BUSY
+}SLAVE_STATE;
+
+/*
+ * Module Definitions
+ */
+Void slaveTask(UArg, UArg);
+int32_t test_multicoreUser (Srio_DrvHandle hSrioDrv);
+
+#endif /* SLAVETASK_H_ */
